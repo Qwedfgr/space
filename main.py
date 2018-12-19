812c898019ca26ce557a9f0f7360bda6aab0aa55
Georgiy Kvelidze <33567245+Qwedfgr@users.noreply.github.com>
2018-12-19 09:51:21 +0300

    Раскомментированы строки.
    Раскомментированы строки.

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import fetch_spacex
 import fetch_hubble
 import instagram
 
+
+fetch_spacex.fetch_spacex_last_launch()
+fetch_hubble.fetch_hubble_collection()
 instagram.post_photos()
-
-#fetch_spacex.fetch_spacex_last_launch()
-
-#fetch_hubble.fetch_hubble_collection()
-
